c7daca24a5842ce3a89286a1f7bd94af83e254f3
Tzunami <tzunamiriverboy@gmail.com>
2016-07-10 17:57:14 -0400

    removed build folder

--- build/libethereum/CMakeFiles/ethereum_BuildInfo.h.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ethereum_BuildInfo.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/ethereum_BuildInfo.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/mix/CMakeFiles/mix-ide_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "mix-ide_automoc.cpp"
-  "CMakeFiles/mix-ide_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/mix-ide_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
